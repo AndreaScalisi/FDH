c021609be5767b43a44ee147a6d49844624ebe61
Andrea <andrea.scalisi@epfl.ch>
2019-11-28 13:59:22 +0100

    Add react base code

---
+++ user-interface/src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const About = () => {
+    return (
+       <div>
+          <h1>About this project</h1>
+           <p>Find more about how we developped this project  <a href="http://fdh.epfl.ch/index.php/Influencers_of_the_past">here</a>.</p>
+       </div>
+    );
+}
+
+export default About;
+
+
---
+++ user-interface/src/components/Paris1884.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Paris1884 = () => {
+    return (
+       <div>
+          <h1>PARIS</h1>
+           <p>Paris 1884 page content</p>
+       </div>
+    );
+}
+
+export default Paris1884;
